ae8ade92a313580a51010dd0561da67422ef83e6
zhosaric <ian.niemeyer16@gmail.com>
2014-04-19 15:54:36 -0400

    Added functionality for Absolute Discounts and invalid inputs
    Good b/c it helps out the user to not crash the program.

--- discount_calculator.py
+++ discount_calculator.py
@@ -1,3 +1,12 @@
 class DiscountCalculator(object):
 	def calculate(self, total, discount_amount, discount_type):
-		return 10.0
+		if discount_type == 'percent':
+			percentage_discount = float(discount_amount)/100
+			discount = float(total) * percentage_discount
+			return discount
+		elif discount_type == 'absolute':
+			discount = discount_amount
+		else:
+			raise ValueError("Invalid discount type")
+		return discount
+		
--- discount_calculator_test.py
+++ discount_calculator_test.py
@@ -6,3 +6,16 @@ class DiscountCalculatorTests(unittest.TestCase):
 		discount_calculator = DiscountCalculator()
 		result = discount_calculator.calculate(100,10,'percent')
 		self.assertEqual(10.0, result)
+		
+	def fifteen_percent_discount_test(self):
+		discount_calculator = DiscountCalculator()
+		result = discount_calculator.calculate(100,15,'percent')
+		self.assertEqual(15.0, result)
+	def five_dollar_discount_test(self):
+		discount_calculator = DiscountCalculator()
+		result = discount_calculator.calculate(250,5,'absolute')
+		self.assertEqual(5, result)
+	def invalid_discount_type_test(self):
+		discount_calculator = DiscountCalculator()
+		self.assertRaises(ValueError, discount_calculator.calculate,
+						 250,5, 'random')
