019908aa9f900f9a965cf0dba31a1e6e5a44d5f4
zhosaric <ian.niemeyer16@gmail.com>
2014-04-19 15:28:38 -0400

    Initial Base Discount Calculator

---
+++ discount_calculator.py
@@ -0,0 +1,3 @@
+class DiscountCalculator(object):
+	def calculate(self, total, discount_amount, discount_type):
+		return 10.0
---
+++ discount_calculator_test.py
@@ -0,0 +1,8 @@
+import unittest
+from discount_calculator import DiscountCalculator
+
+class DiscountCalculatorTests(unittest.TestCase):
+	def test_ten_percent_discount(self):
+		discount_calculator = DiscountCalculator()
+		result = discount_calculator.calculate(100,10,'percent')
+		self.assertEqual(10.0, result)
